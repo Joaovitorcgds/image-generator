7374c9755cca8ed41931e6895e3edcf5cb571c65
joaov <joaovitorcgds@gmail.com>
2022-03-19 01:36:38 -0300

    mobile 0.1

--- src/components/ImageHtml.js
+++ src/components/ImageHtml.js
@@ -14,7 +14,7 @@ export default function ImageHtml({
       <CreatedImage id="imageHtml" color={`#${bgColor}`}
         backgroundColor={`linear-gradient(to left, #${bgColor}, #000000)`}>
         <Content>
-          <img src={`./images/${tech}-logo.png`} alt="logo react" width="180px"/>
+          <Image src={`./images/${tech}-logo.png`} alt="logo react"/>
           <div>
             <Title colorText={`#${colorText}`}>{office}</Title>
             <Ul>
@@ -48,6 +48,40 @@ const CreatedImage = styled.div.attrs(({backgroundColor}) => ({
   align-items: center;
   padding-left: 260px;
   box-sizing: border-box;
+
+  @media(max-width: 998px){
+    width: 95vw;
+    height: 25vh;
+  }
+
+  @media(max-width: 890px){
+    padding-left: 230px;
+  }
+
+  @media(max-width: 842px){
+    padding-left: 210px;
+  }
+
+  @media(max-width: 810px){
+    padding-left: 190px;
+  }
+
+  @media(max-width: 731px){
+    padding-left: 170px;
+  }
+
+  @media(max-width: 425px){
+    padding-left: 80px;
+  }
+
+  @media(max-width: 375px){
+    padding-left: 70px;
+  }
+
+  @media(max-width: 330px){
+    padding-left: 60px;
+  }
+
 `
 
 const Content = styled.div`
@@ -61,18 +95,119 @@ const Content = styled.div`
   div{
     width: 470px;
   }
+
+  @media(max-width: 998px){
+    max-width: 65vw;
+
+    div{
+      width: 43vw;
+    }
+  }
+
+  @media(max-width: 840px){
+    div{
+      width: 45vw;
+    }
+  }
+
+  @media(max-width: 810px){
+    max-width: 70vw;
+    
+    div{
+      width: 50vw;
+    }
+  }
+
+  @media(max-width: 720px){
+    
+    div{
+      width: 53vw;
+    }
+  }
+
+  @media(max-width: 425px){
+    max-width: 78vw;
+    gap: 10px;
+  }
+
+  @media(max-width: 375px){
+    max-width: 78vw;
+    gap: 10px;
+
+    div{
+      width: 49vw;
+    }
+  }
+
+  @media(max-width: 330px){
+    max-width: 80vw;
+    gap: 10px;
+
+    div{
+      width: 48vw;
+    }
+  }
+
 `
+
+const Image = styled.img`
+  width: 180px;
+  
+  @media(max-width: 998px){
+    width: 17vw;
+  }
+
+  @media(max-width: 425px){
+    width: 25vw;
+  }
+
+  @media(max-width: 375px){
+    width: 25vw;
+  }
+
+  @media(max-width: 323px){
+    width: 25vw;
+  }
+
+`
+
 const Title = styled.h1.attrs(({colorText}) => ({
     style: { color: colorText }
   })
 )`
   font-size: 25px;
+
+  @media(max-width: 998px){
+    font-size: 20px;
+  }
+
+  @media(max-width: 715px){
+    font-size: 17px;
+  }
+
+  @media(max-width: 425px){
+    font-size: 11px;
+  }
+
+  @media(max-width: 375px){
+    font-size: 10px;
+  }
+
+  @media(max-width: 320px){
+    font-size: 8px;
+  }
+
+
 `
 const Ul = styled.ul`
   display: flex;
   justify-content: space-between;
   padding-right: 20px;
   margin-top: 60px;
+
+  @media(max-width: 320px){
+    margin-top: 40px;
+  }
 `
 
 const SubTitle = styled.li`
@@ -80,5 +215,25 @@ const SubTitle = styled.li`
   list-style: none;
   font-size: 23px;
   font-weight: bold;
-  color: ${props => props.color}
+  color: ${props => props.color};
+
+  @media(max-width: 998px){
+    font-size: 18px;
+  }
+
+  @media(max-width: 715px){
+    font-size: 15px;
+  }
+
+  @media(max-width: 425px){
+    font-size: 10px;
+  }
+
+  @media(max-width: 375px){
+    font-size: 8px;
+  }
+
+  @media(max-width: 320px){
+    font-size: 6px;
+  }
 `
--- src/components/Options.js
+++ src/components/Options.js
@@ -56,59 +56,60 @@ export default function Options({
  
   return(
     <Form onSubmit={(e) => e.preventDefault()}>
-      <Cor>
+      <Container>
+        <Cor>
+          <InputType>
+            <input
+              type="color" id="background"
+              name="background" value={`#${bgColor}`} onChange={e => bgColorSelect(e)}/>
+            <label htmlFor="background">Cor de fundo</label>
+          </InputType>
+          <InputType>
+            <input
+              type="color" name="colorText"
+              value={`#${colorText}`} onChange={e => colorTextSelect(e)}/>
+            <label htmlFor="colorText">Cor da fonte</label>
+          </InputType>
+        </Cor>
         <InputType>
-          <input
-            type="color" id="background"
-            name="background" value={`#${bgColor}`} onChange={e => bgColorSelect(e)}/>
-          <label htmlFor="background">Cor de fundo</label>
+          <select name="tech" onChange={(e) => getTech(e)}>
+            {Object.entries(technology).map((key, value) => {
+              return(
+                <option value={key[0]} key={value}>{key[1]}</option>
+              )
+            })}
+          </select>
+          <label htmlFor="tech">Tech</label>
         </InputType>
-        <InputType>
-          <input
-            type="color" name="colorText"
-            value={`#${colorText}`} onChange={e => colorTextSelect(e)}/>
-          <label htmlFor="colorText">Cor da fonte</label>
-        </InputType>
-      </Cor>
-
-      <InputType>
-        <select name="tech" onChange={(e) => getTech(e)}>
-          {Object.entries(technology).map((key, value) => {
-            return(
-              <option value={key[0]} key={value}>{key[1]}</option>
-            )
-          })}
-        </select>
-        <label htmlFor="tech">Tech</label>
-      </InputType>
+      </Container>
       
-      <InputType>
-        <Multiselect className="searchWrapper"
-          options={options} 
-          displayValue="country" 
-          selectionLimit={3}
-          onSelect={(selectedList) => setTechList([selectedList])}
-          placeholder="Escolha 3 skills"
-          style={{
-            multiselectContainer: {width: "250px", padding: 0, 
-            border: "1px black solid"},
-            searchBox:{ border: "none", padding: 0, textAlign:"center"},
-            chips: { background: 'black', fontSize: "1rem", padding: "5px",
-            margin: "3px"},
-            optionContainer: { border: "none"},
-            inputField: {padding: 0, margin: 0, fontSize: "1.5rem"},
-            option: {color: "black", fontSize: "1.2rem",textAlign: "center"
-            ,borderLeft: "1px black solid", borderRight: "1px black solid"}
-
-          } }/>
-        <label htmlFor="sub-tech">Skills</label>
-      </InputType>
-
-      <InputType>
-        <input type="text" name="cargo" 
-          value={office} onChange={e => profession(e)}/>
-        <label htmlFor="cargo">Cargo</label>
-      </InputType>
+      <Container2>
+        <InputType>
+          <Multiselect className="searchWrapper"
+            options={options}
+            displayValue="country"
+            selectionLimit={3}
+            onSelect={(selectedList) => setTechList([selectedList])}
+            placeholder="Escolha 3 skills"
+            style={{
+              multiselectContainer: {width: "250px", padding: 0,
+              border: "1px black solid"},
+              searchBox:{ border: "none", padding: 0, textAlign:"center"},
+              chips: { background: 'black', fontSize: "1rem", padding: "5px",
+              margin: "3px"},
+              optionContainer: { border: "none"},
+              inputField: {padding: 0, margin: 0, fontSize: "1.5rem"},
+              option: {color: "black", fontSize: "1.2rem",textAlign: "center"
+              ,borderLeft: "1px black solid", borderRight: "1px black solid"}
+            } }/>
+          <label htmlFor="sub-tech">Skills</label>
+        </InputType>
+        <InputType>
+          <input type="text" name="cargo"
+            value={office} onChange={e => profession(e)}/>
+          <label htmlFor="cargo">Cargo</label>
+        </InputType>
+      </Container2>
     </Form>
   )
 }
@@ -116,17 +117,54 @@ export default function Options({
 
 const Form = styled.form`
   display: flex;
-  justify-content: space-between;
-  align-items: flex-start;
+  justify-content: space-around;
+  align-items: center;
   max-width: 747px;
   margin: auto;
   margin-bottom: 20px;
+
+  @media(max-width: 390px){
+    flex-direction: column;
+    row-gap: 15px;
+  }
+
 `;
 
 const Cor = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: 15px;
+
+  @media(max-width: 390px){
+    width: 75vw;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 40vw;
+
+  @media(max-width: 500px){
+    flex-direction: column;
+    row-gap: 15px;
+  }
+
+`
+
+const Container2 = styled.div`
+  display: flex;
+  gap: 10px;
+
+  align-items: flex-start;
+  flex-direction: column-reverse;
+  @media(max-width: 660px){
+    
+    
+  }
 `
 
 const InputType = styled.div`
@@ -143,7 +181,7 @@ const InputType = styled.div`
   }
 
   input[type= text]{
-    width: 20rem;
+    width: 25rem;
   }
 
   label{
